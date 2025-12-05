87fcf5a24b45257139f2167e2ac9674cb3c95844
amirtalha <amir.talha@gmail.com>
2025-12-05 21:15:21 +0530

    update app version4

--- version/app.js
+++ version/app.js
@@ -5,13 +5,14 @@ var response;
 
 app.get('/', function (req, res) {
 
-  response = 'This is version 2 of the app.' + '\n';
+  response = 'This is version 4 of the app.' + '\n';
 
   //send the response to the client
   res.send(response);
 
 });
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
 });
